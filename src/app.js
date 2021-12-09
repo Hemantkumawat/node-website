6a72c79cfe54862ad571a326e3dddba7b4c6d437
Hemant Kumawat <hemantkumawat275@gmail.com>
2021-12-09 08:41:35 +0530

    Port name issue resolved

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const { response } = require('express')
 const { request } = require('http')
 
 const app= express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // nodemon src/app.js -e js,hbs
 // Define paths for 
