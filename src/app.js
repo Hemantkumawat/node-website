394eb49c3b8aece7b3733bc7a3658f99d791f287
Hemant Kumawat <hemantkumawat275@gmail.com>
2021-12-09 08:53:21 +0530

    Geocode Forecast Configuration done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const hbs = require('hbs')
 const { response } = require('express')
 const { request } = require('http')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app= express()
 const port = process.env.PORT || 3000
@@ -46,4 +48,31 @@ app.get('*',(req,res)=>{
 
 app.listen(port,()=>{
     console.log('Server is up on port '+ port)
+})
+
+
+app.get('/get-weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
 })
